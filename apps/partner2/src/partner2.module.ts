faf79418383548080802ac19d122e8a4bd560377
Pedro Furlan <pedrofurlan6@gmail.com>
2024-06-21 10:46:56 -0300

    feat: finishing partners api

--- apps/partner2/src/partner2.module.ts
+++ apps/partner2/src/partner2.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { Partner2Controller } from './partner2.controller';
-import { Partner2Service } from './partner2.service';
+import { ConfigModule } from '@nestjs/config';
+import { EventosModule } from './eventos/eventos.module';
+import { LugaresModule } from './lugares/lugares.module';
+import { PrismaModule } from '@app/core/prisma/prisma.module';
+import { AuthModule } from '@app/core/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [Partner2Controller],
-  providers: [Partner2Service],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env.partner2', isGlobal: true }),
+    PrismaModule,
+    EventosModule,
+    AuthModule,
+    LugaresModule,
+  ],
 })
 export class Partner2Module {}
